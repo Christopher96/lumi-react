7097c9874f20ebc4d987f9afdc58b651011552c5
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-05-07 10:41:42 +0200

    template component

---
+++ src/components/template/template-component.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+interface IProps {}
+interface IState {}
+
+export default class UserComponent extends Component<IProps, IState> {
+  render() {
+    return <></>;
+  }
+}
