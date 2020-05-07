d46d95e40f8738b853280a1daabec9ad87227daf
taumich <michael.arenander@gmail.com>
2020-05-07 14:30:44 +0200

    server log system visualized

--- src/components/server-log/server-log-component.tsx
+++ src/components/server-log/server-log-component.tsx
@@ -1,27 +1,67 @@
 import React, { Component } from "react";
-import { Row, Button, Radio, Timeline } from "antd";
+import { Row, Button, Radio, Timeline, Col, message } from "antd";
 
 interface IProps {}
-interface IState {}
+interface IState {
+  timestamp: string;
+  log: string;
+}
+
+const key = "updatable";
 
 export default class ServerLogComponent extends Component<IProps, IState> {
-  state = {};
+  state = { timestamp: "2020-01-01", log: "_" };
+
+  val1 = [
+    "2020-01-01 20:20:0:0",
+    "hej jag heter Michael och kan skriva massa logs i en string",
+  ];
+  val2 = ["2020-01-01 20:20:0:0", "hej jag testar skriva"];
+  val3 = ["2020-01-01 20:20:0:0", "hej äter mat"];
+  testTree = [
+    this.val1,
+    this.val2,
+    this.val3,
+    this.val1,
+    this.val2,
+    this.val3,
+    this.val1,
+    this.val2,
+    this.val3,
+    this.val1,
+    this.val2,
+    this.val3,
+    this.val1,
+    this.val2,
+    this.val3,
+  ];
+
+  makeLog = (time: string, text: string) => {
+    return <Timeline.Item label={time}>{text}</Timeline.Item>;
+  };
+
+  onExport = () => {
+    message.loading({ content: "Exporting...", key });
+    setTimeout(() => {
+      message.success({ content: "Exported!", key, duration: 2 });
+    }, 1000);
+  };
 
   render() {
     return (
-      <div>
+      <>
         <h2>Server Log:</h2>
-        <Timeline mode="left">
-          <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Solve initial network problems
-          </Timeline.Item>
-          <Timeline.Item>Technical testing</Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11">
-            Network problems being solved
-          </Timeline.Item>
-        </Timeline>
-      </div>
+        <div className="log-window">
+          <Timeline mode="left">
+            {this.testTree.map((element) => {
+              return this.makeLog(element[0], element[1]);
+            })}
+          </Timeline>
+        </div>
+        <div className="footer">
+          <Button onClick={this.onExport}>Export Log</Button>
+        </div>
+      </>
     );
   }
 }
